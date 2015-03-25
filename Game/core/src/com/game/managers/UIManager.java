be19d8673b873a3007a2d5243daa252c4ffdb382
Rhiyo <Rhiyofromizal@hotmail.com>
2015-03-26 06:57:21 +1030

    UI Update
    Added Touchpad and spell casting button for UI

--- Game/core/src/com/game/managers/UIManager.java
+++ Game/core/src/com/game/managers/UIManager.java
@@ -1,7 +1,89 @@
 package com.game.managers;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+
 /**
  * Created by Keirron on 22/03/2015.
  */
-public class UIManager {
+public class UIManager{
+
+    private Stage stage;
+
+    private Skin skin;
+
+    private Touchpad touchpad;
+    private Touchpad.TouchpadStyle touchpadStyle;
+    private Skin touchpadSkin;
+    private Drawable touchBackground;
+    private Drawable touchKnob;
+
+    private TextButton castSpellBtn;
+
+    public UIManager(){ // TODO Screen width/height inputs to size the interace correctly
+
+        //Set up Stage
+        stage = new Stage();
+        //stage.getViewport().update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), true);
+
+        //Set UI Skins
+        skin = new Skin(Gdx.files.internal("UI/uiskin.json"));
+
+        //Cast Spell Button
+        castSpellBtn = new TextButton("Cast Spell",skin);
+        castSpellBtn.setPosition(200, 100);
+        castSpellBtn.setScale(5, 5);
+        castSpellBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                //
+            }
+        });
+
+
+        //Touchpad
+        touchpadSkin = new Skin();
+        touchpadSkin.add("touchBackground", new Texture("UI/touchpad/touchBackground.png"));
+        touchpadSkin.add("touchKnob", new Texture("UI/touchpadtouchKnob.png"));
+        touchpadStyle = new Touchpad.TouchpadStyle();
+        touchBackground = touchpadSkin.getDrawable("touchBackground");
+        touchKnob = touchpadSkin.getDrawable("touchKnob");
+        //Apply the Drawables to the TouchPad Style
+        touchpadStyle.background = touchBackground;
+        touchpadStyle.knob = touchKnob;
+        touchpad = new Touchpad(10, touchpadStyle);
+        touchpad.setBounds(15, 15, 200, 200);
+        touchpad.setPosition(100,100);
+
+        //Add components to stage
+        stage.addActor(castSpellBtn);
+        stage.addActor(touchpad);
+    }
+
+    public float getKnobPercentY(){
+        return touchpad.getKnobPercentY();
+    }
+
+    public float getKnobPercentX(){
+        return touchpad.getKnobPercentX();
+    }
+
+    public void render(){
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+    }
+
+    public void dispose(){
+        stage.dispose();
+        skin.dispose();
+        touchpadSkin.dispose();
+    }
+
 }
